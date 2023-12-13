d307ec8f173febded4e7e96f9d556775153af9f8
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-13 15:29:56 -0800

    add event listeners to listen to storage change

--- popup.js
+++ popup.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.sync.get({basket: []}, function(result) {
+        let basket = result.basket;
+        let list = document.getElementById('productList'); // An element with id 'productList' in your popup.html
+        basket.forEach(productName => {
+            let listItem = document.createElement('li');
+            listItem.textContent = productName;
+            list.appendChild(listItem);
+        });
+    });
+});
+
 const selectProduct = () => {
     console.log("product selected ")
 }
