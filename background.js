dee7572e227e24f2232b284ef9f6f5b5ba6ce6b1
Mohammad Rad <mesmaeil@geeksquad.ca>
2023-12-13 18:16:01 -0800

    add product specs fetching process

--- background.js
+++ background.js
@@ -9,4 +9,25 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
         });
     }
   });
-  
+
+
+// Fetching product spects
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'fetchProductDetails') {
+        const urls = request.urls;
+        urls.forEach(url => {
+            chrome.tabs.create({ url: url, active: false }, tab => {
+                // Wait for the tab to load and then inject the content script
+                chrome.tabs.onUpdated.addListener(function onUpdated(tabId, info) {
+                    if (info.status === 'complete' && tabId === tab.id) {
+                        chrome.tabs.executeScript(tab.id, {file: 'contentScript.js'}, () => {
+                            // Optionally close the tab after scraping
+                            chrome.tabs.remove(tab.id);
+                        });
+                        chrome.tabs.onUpdated.removeListener(onUpdated);
+                    }
+                });
+            });
+        });
+    }
+});
